76e4bf3a6e8c33893742729d40def0f21b1aef11
easabbir <easabbir@gmail.com>
2023-09-19 16:02:21 +0600

    initial commit

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,12 @@
+const cypress = require("cypress")
+
+describe('suite name', () => {
+    
+    it('test1', () => {
+        cy.visit("https://app.dev.shadowchef.co/login")
+        
+        
+      
+    })
+
+})
---
+++ cypress/e2e/reservation.cy.js
@@ -0,0 +1,10 @@
+
+const cypress = require ("cypress")
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit("https://app.dev.shadowchef.co/login")
+    
+    
+  })
+})
