c182098023d85d9053bd082ca6d9ba51b8ba4e90
easabbir <easabbir@gmail.com>
2023-09-21 17:30:49 +0600

    removed unnecessary tracking

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,16 @@
 import Login from "../pageObjects/loginPage"
 
-describe('suite name', () => {
+describe('Reservation', () => {
     
-    it('test1', () => {
+    it('login', () => {
         cy.visit("https://app.dev.shadowchef.co/login")
         const ln=new Login();
 
         ln.setEmail('ehsanul.alam+11@vivacomsolutions.com')
         ln.setPassword('Abbl_123')
         ln.clickButton
+
+        //cy.url().should('include','shadowchef.co')
         
         // cy.title()
         // { timeout: 10000 }.should('eq', 'Order Dashboard')
